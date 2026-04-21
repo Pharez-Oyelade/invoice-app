208301e9915e75136625d6366d2108ee93407155
Pharez <pharezoyelade@gmail.com>
2026-04-21 14:37:05 +0100

    feat(ui): add invoice cards to invoice list page

--- src/components/invoice/InvoiceCard.tsx
+++ src/components/invoice/InvoiceCard.tsx
@@ -1,15 +1,29 @@
+import { ChevronRight } from "lucide-react";
+
 interface InvoiceCardProps {
   id: string;
   date: string;
   name: string;
   status: string;
   total: number;
+  onClick: () => void;
 }
 
-const InvoiceCard = ({ id, date, name, status, total }: InvoiceCardProps) => {
+const InvoiceCard = ({
+  id,
+  date,
+  name,
+  status,
+  total,
+  onClick,
+}: InvoiceCardProps) => {
   return (
-    <div className="flex justify-between bg-white px-[32px] items-center w-full h-[72px] rounded-[8px] mt-[264px]">
-      <div>
+    <div
+      onClick={onClick}
+      className="flex justify-between bg-white px-[32px] items-center w-full h-[72px] rounded-[8px] mt-5 shadow-card cursor-pointer
+"
+    >
+      <div className="flex gap-10 items-center">
         <h2 className="font-bold text-[15px] ">
           <span className="text-[#ccc]">#</span> {id}
         </h2>
@@ -17,21 +31,24 @@ const InvoiceCard = ({ id, date, name, status, total }: InvoiceCardProps) => {
         <p className="text-[13px] text-[#858BB2]">{name}</p>
       </div>
 
-      <div>
-        <div className="font-bold text-[15px]">${total.toFixed(2)}</div>
+      <div className="flex gap-10 items-center">
+        <h2 className="font-bold text-[15px]">${total.toFixed(2)}</h2>
         <div
-          className={`px-[8px] py-[4px] rounded-full text-[12px] font-bold ${
-            status === "Paid"
+          className={`px-[8px] py-[4px] w-[104px] h-[40px] text-center text-[15px] font-bold flex justify-center items-center gap-2 capitalize ${
+            status === "paid"
               ? "bg-green-100 text-green-500"
-              : status === "Pending"
+              : status === "pending"
                 ? "bg-yellow-100 text-yellow-500"
                 : "bg-red-100 text-red-500"
           }`}
         >
           <div
-            className={`h-4 w-4 ${status === "Paid" ? "bg-green-500" : status === "Pending" ? "bg-yellow-500" : "bg-red-500"} rounded-full inline-block mr-2`}
+            className={`h-2 w-2 ${status === "paid" ? "bg-green-500" : status === "pending" ? "bg-yellow-500" : "bg-red-500"} rounded-full inline-block mr-2`}
           />
           {status}
+        </div>
+        <div>
+          <ChevronRight className="w-4 h-4" />
         </div>
       </div>
     </div>
--- src/pages/InvoiceListPage.tsx
+++ src/pages/InvoiceListPage.tsx
@@ -1,12 +1,16 @@
 import Button from "../components/ui/Button";
 import { useInvoices } from "../context/InvoiceContext";
+import InvoiceCard from "../components/invoice/InvoiceCard";
+import { useNavigate } from "react-router-dom";
 
 const InvoiceListPage = () => {
   const { invoices } = useInvoices();
+  const navigate = useNavigate();
+
   return (
-    <main className="bg-(--bg-primary) flex justify-center items-center mt-[77px] w-full">
+    <main className="bg-(--bg-primary) flex flex-col justify-center items-center mt-[77px] w-full">
       <header className="flex justify-between items-center w-full">
-        <div className="leading-tight space-y-[1px] flex items-start gap-0 flex-col">
+        <div className="space-y-3 flex items-start gap-0 flex-col">
           <h1 className="leading-[100%] tracking-[-1.13px] ">Invoices</h1>
           <p className="text-(--text-secondary)">
             There are {invoices.length} total invoices
@@ -18,6 +22,20 @@ const InvoiceListPage = () => {
           <Button variant="primary" text="New Invoice" />
         </div>
       </header>
+
+      <section className="w-full mt-[48px]">
+        {invoices.map((invoice) => (
+          <InvoiceCard
+            key={invoice.id}
+            id={invoice.id}
+            date={invoice.paymentDue}
+            name={invoice.clientName}
+            total={invoice.total}
+            status={invoice.status}
+            onClick={() => navigate(`/invoice/${invoice.id}`)}
+          />
+        ))}
+      </section>
     </main>
   );
 };
