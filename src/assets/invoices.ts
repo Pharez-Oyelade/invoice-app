681a3d92300cb38a88fbc2e143f57919e433ae8c
Pharez <pharezoyelade@gmail.com>
2026-04-21 13:45:35 +0100

    feat(data): create invoice data

---
+++ src/assets/invoices.ts
@@ -0,0 +1,51 @@
+export const invoiceData = [
+  {
+    id: "RT3080",
+    createdAt: "2021-08-18",
+    paymentDue: "2021-09-19",
+    description: "Re-branding",
+    paymentTerms: 30,
+    clientName: "Jensen Huang",
+    clientEmail: "jensenh@mail.com",
+    status: "paid",
+    senderAddress: {
+      street: "19 Union Terrace",
+      city: "London",
+      postCode: "E1 3EZ",
+      country: "United Kingdom",
+    },
+    clientAddress: {
+      street: "106 Kendell Street",
+      city: "SharBorough",
+      postCode: "S1 0JY",
+      country: "United Kingdom",
+    },
+    items: [{ name: "Brand Guidlines", quantity: 1, price: 1800.9 }],
+    total: 1800.9,
+  },
+
+  {
+    id: "RT3080",
+    createdAt: "2021-08-18",
+    paymentDue: "2021-09-19",
+    description: "Re-branding",
+    paymentTerms: 30,
+    clientName: "Jensen Huang",
+    clientEmail: "jensenh@mail.com",
+    status: "paid",
+    senderAddress: {
+      street: "19 Union Terrace",
+      city: "London",
+      postCode: "E1 3EZ",
+      country: "United Kingdom",
+    },
+    clientAddress: {
+      street: "106 Kendell Street",
+      city: "SharBorough",
+      postCode: "S1 0JY",
+      country: "United Kingdom",
+    },
+    items: [{ name: "Brand Guidlines", quantity: 1, price: 1800.9 }],
+    total: 1800.9,
+  },
+];
